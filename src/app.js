9df36820546da65920f206c5db7bafaffc301633
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-10-28 12:09:34 -0500

    app correction

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from "express"
 import employeesRoutes from "./routes/employes.routes.js"
 import imdexRoutes from "./routes/index.routes.js"
 
-import { PORT } from "./config.js"
-
 const app = express()
 
 app.use(express.json())
@@ -16,6 +14,4 @@ app.use((req, res, next) => {
 	res.status(404).json({ message: "endpoint not found" })
 })
 
-app.listen(PORT)
-
-console.log("server on port " + PORT)
+export default app
